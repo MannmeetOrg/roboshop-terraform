b84fd20d0668fbaa3557c1ceddd1973b13259de1
Mannmeet <manmeet.a.jb@gmail.com>
2024-09-26 21:11:43 +0530

    Roboshop VPC configuration modified

--- main.tf
+++ main.tf
@@ -2,16 +2,16 @@
 module "vpc" {
   source = "./modules/vpc"
 
-  cidr                = var.vpc["cidr"]
-  env                 = var.env
-  public_subnet       = var.vpc["public_subnet"]
-  web_subnet          = var.vpc["web_subnet"]
-  app_subnet          = var.vpc["app_subnet"]
-  db_subnet           = var.vpc["db_subnet"]
-  availability_zones  = var.vpc["availability_zones"]
-  default_vpc_id      = var.vpc["default_vpc_id"]
-  default_vpc_rt      = var.vpc["default_vpc_rt"]
-  default_vpc_cidr    = var.vpc["default_vpc_cidr"]
+  cidr                 = var.vpc["cidr"]
+  env                  = var.env
+  public_subnets       = var.vpc["public_subnets"]
+  web_subnets          = var.vpc["web_subnets"]
+  app_subnets          = var.vpc["app_subnets"]
+  db_subnets           = var.vpc["db_subnets"]
+  availability_zones   = var.vpc["availability_zones"]
+  default_vpc_id       = var.vpc["default_vpc_id"]
+  default_vpc_rt       = var.vpc["default_vpc_rt"]
+  default_vpc_cidr     = var.vpc["default_vpc_cidr"]
 }
 
 # module "ec2" {
